40b1f33f828814d26239d6be2d22317b1b5377dc
송이현 <songehyun00@gmail.com>
2024-07-11 17:18:08 +0900

    4번 img 태그에 classname 추가

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,11 @@ import Imgmake from "./tagmaker/imgtagmaker";
 const App: React.FC = () => {
   return (
     <div>
-      <Imgmake srcs={"../img/backimage.jfif"} alts={"메인이미지야"} />
+      <Imgmake
+        srcs={"../img/backimage.jfif"}
+        alts={"메인이미지야"}
+        calssname={"test"}
+      />
     </div>
   );
 };
--- src/tagmaker/imgtagmaker.tsx
+++ src/tagmaker/imgtagmaker.tsx
@@ -1,10 +1,11 @@
 interface ImgMakerPros {
   srcs: string;
   alts: string;
+  calssname: string;
 }
 
-const Imgmake: React.FC<ImgMakerPros> = ({ srcs, alts }) => {
-  return <img src={srcs} alt={alts} />;
+const Imgmake: React.FC<ImgMakerPros> = ({ srcs, alts, calssname }) => {
+  return <img src={srcs} alt={alts} className={calssname} />;
 };
 
 export default Imgmake;
