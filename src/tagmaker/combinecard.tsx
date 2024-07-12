7ac903ecee2d846bfd770f6591e9ac61b43c950f
송이현 <songehyun00@gmail.com>
2024-07-12 09:20:36 +0900

    4번 리터럴 정리

--- src/tagmaker/combinecard.tsx
+++ src/tagmaker/combinecard.tsx
@@ -3,43 +3,15 @@ import Divmaker from "./divmaker";
 import Hrmaker from "./hrmaker";
 import H2maker from "./h2maker";
 import Pmaker from "./pmaker";
+import { Maketext, cardtitle } from "../../static/lorem";
 
 const Combine: React.FC = () => {
   return (
     <Divmaker classname={"card"}>
       <Divmaker classname={"card-container"}>
         <Hrmaker classname={"card-line"} />
-        <H2maker classname={"card-title"}>INDENTATION</H2maker>
-        <Pmaker classname={"card-text"}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, corporis
-          quasi consequatur tempore voluptas explicabo dolorem sed quam
-          cupiditate ipsa non magnam delectus distinctio, nemo ratione, eligendi
-          maxime molestiae quae.Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Ex, corporis quasi consequatur tempore voluptas
-          explicabo dolorem sed quam cupiditate ipsa non magnam delectus
-          distinctio, nemo ratione, eligendi maxime molestiae quae.Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Ex, corporis quasi
-          consequatur tempore voluptas explicabo dolorem sed quam cupiditate
-          ipsa non magnam delectus distinctio, nemo ratione, eligendi maxime
-          molestiae quae.Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Ex, corporis quasi consequatur tempore voluptas explicabo
-          dolorem sed quam cupiditate ipsa non magnam delectus distinctio, nemo
-          ratione, eligendi maxime molestiae quae.Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Ex, corporis quasi consequatur tempore
-          voluptas explicabo dolorem sed quam cupiditate ipsa non magnam
-          delectus distinctio, nemo ratione, eligendi maxime molestiae
-          quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex,
-          corporis quasi consequatur tempore voluptas explicabo dolorem sed quam
-          cupiditate ipsa non magnam delectus distinctio, nemo ratione, eligendi
-          maxime molestiae quae.Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Ex, corporis quasi consequatur tempore voluptas
-          explicabo dolorem sed quam cupiditate ipsa non magnam delectus
-          distinctio, nemo ratione, eligendi maxime molestiae quae.Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Ex, corporis quasi
-          consequatur tempore voluptas explicabo dolorem sed quam cupiditate
-          ipsa non magnam delectus distinctio, nemo ratione, eligendi maxime
-          molestiae quae.
-        </Pmaker>
+        <H2maker classname={"card-title"}>{cardtitle}</H2maker>
+        <Pmaker classname={"card-text"}>{Maketext}</Pmaker>
       </Divmaker>
     </Divmaker>
   );
--- src/tagmaker/combinecard2.tsx
+++ src/tagmaker/combinecard2.tsx
@@ -3,19 +3,15 @@ import Divmaker from "./divmaker";
 import Hrmaker from "./hrmaker";
 import H2maker from "./h2maker";
 import Pmaker from "./pmaker";
+import { Maketext, card2title } from "../../static/lorem";
 
 const Combine2: React.FC = () => {
   return (
     <Divmaker classname={"card2"}>
       <Divmaker classname={"card-container2"}>
         <Hrmaker classname={"card-line2"} />
-        <H2maker classname={"card-title2"}>EXCLUSIVE</H2maker>
-        <Pmaker classname={"card-text2"}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, corporis
-          quasi consequatur tempore voluptas explicabo dolorem sed quam
-          cupiditate ipsa non magnam delectus distinctio, nemo ratione, eligendi
-          maxime molestiae quae.
-        </Pmaker>
+        <H2maker classname={"card-title2"}>{card2title}</H2maker>
+        <Pmaker classname={"card-text2"}>{Maketext}</Pmaker>
       </Divmaker>
     </Divmaker>
   );
